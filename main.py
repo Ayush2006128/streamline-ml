438727544d8f253810b7641c4455d6792cb485e4
”Ayush <ayush.muley1907@outlook.com>
2025-04-28 04:59:27 +0000

    streamlineML is  coming!!!

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import streamlit as st
+import time
 # TODO: WORK ON THIS
+st.title("streamlineML")
+st.info("Somthing cool is coming soon!")
+
+def main():
+    if int(time.time()) % 2 == 0:
+        st.balloons()
+
+while True:
+    main()
+    time.sleep(1)
+    st.rerun()
